492da64291307f34196bee243c22c2b8d133c168
quispedavid987 <quispedavid987@gmail.com>
2025-12-01 00:52:55 -0500

    augmentation

--- detectorDNN.py
+++ detectorDNN.py
@@ -1,7 +1,7 @@
 import cv2
 import os
 import time
-import albumentations as A #---- 
+import albumentations as A #---- AGREGANDO ALBUMENTATIONS PARA ROTACIONES
 # ---- ARQUITECTURA
 prototxt = "models/deploy.prototxt"
 
@@ -13,6 +13,9 @@ net = cv2.dnn.readNetFromCaffe(prototxt, model)
 
 images = sorted(os.listdir('./images/'))
 
+# ROTAMOS, CAMBIAMOS EL BRILLO Y SIMULAMOS OCLUSION
+transform = A.Compose([A.SafeRotate(limit=30,p=1.0),A.RandomBrightnessContrast(p=1.0),A.CoarseDropout(num_holes_range=(1,2),hole_height_range=(10,20),hole_width_range=(10,20),p=1.0),])
+
 time_cumulated = 0
 confidence = 0.90
 print(f"Grado de confianza en detección : {confidence}")
@@ -22,9 +25,19 @@ for img in images:
 	## LEYENDO UNA IMAGEN
 	image = cv2.imread(f"images/{img}")
 
+	# ---- APLICAMOS AUGMENTATION
+	## CONVERTIMOS A RGB
+	image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+
+	# ---- APLICAMOS LA TRANSFORMACION
+	augmented = transform(image=image_rgb)["image"]
+
+	# ---- REGRESAMOS AL FORMATO PARA EL MODELO DNNS
+	image_def = cv2.cvtColor(augmented, cv2.COLOR_RGB2BGR)
+
 	## REDIMENSIONANDO PARA QUE SEA IMAGEN FUNCIONAL CON LA RED
-	height, width, _ = image.shape
-	image_resized = cv2.resize(image, (300,300))
+	height, width, _ = image_def.shape
+	image_resized = cv2.resize(image_def, (300,300))
 
 	## PREPROCESAMIENTO
 	blob = cv2.dnn.blobFromImage(image_resized, 1, (300,300), (104, 117, 123)) # PARAMETROS STANDAR PARA LA RED
@@ -39,8 +52,8 @@ for img in images:
 		if detection[2] > confidence: ## CAMBIANDO EL VALOR DE CONFIANZA
 			box = detection[3:7]*[width, height, width, height] ## SE EXTRAE LAS COORDENADAS DE LA DETECCION
 			x_start, y_start, x_end, y_end = int(box[0]), int(box[1]), int(box[2]), int(box[3])
-			cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0,0,255), 2)
-			cv2.putText(image, f"{detection[2]*100:.1f}", (x_start, y_start-5), 1, 1.2, (0,0,255), 2)
+			cv2.rectangle(image_def, (x_start, y_start), (x_end, y_end), (0,0,255), 2)
+			cv2.putText(image_def, f"{detection[2]*100:.1f}", (x_start, y_start-5), 1, 1.2, (0,0,255), 2)
 			detected_faces += 1
 
 	end = time.time()
@@ -48,7 +61,7 @@ for img in images:
 	print(f'{img}\t{(end-start)*1000:.2f}\t{detected_faces}\t')
 	## MOSTRANDO IMAGEN
 
-	cv2.imwrite(f'results/{img}_cfd_0{confidence*100}.jpg',image)
+	cv2.imwrite(f'results_aug/{img}_cfd_0{confidence*100}.jpg',image_def)
 	#cv2.imshow("Imagen", image)
 	#cv2.imshow("Imagen muestra", image_show)
 	#cv2.imshow("Imagen redimensionada", image_resized)
