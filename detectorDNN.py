b5d93f17a1055cf143c2c01745d8e1926045f0a6
quispedavid987 <quispedavid987@gmail.com>
2025-11-30 19:10:04 -0500

    Agregando valores DNN

--- detectorDNN.py
+++ detectorDNN.py
@@ -1,5 +1,6 @@
 import cv2
 import os
+import time
 # ---- ARQUITECTURA
 prototxt = "models/deploy.prototxt"
 
@@ -9,9 +10,14 @@ model = "models/res10_300x300_ssd_iter_140000.caffemodel"
 # --- CARGANDO EL MODELO
 net = cv2.dnn.readNetFromCaffe(prototxt, model)
 
-images = os.listdir('./images/')
+images = sorted(os.listdir('./images/'))
 
+time_cumulated = 0
+confidence = 0.30
+print(f"Grado de confianza en detección : {confidence}")
+print(f"Imagen\tTiempo( ms)\tRostros")
 for img in images:
+	start = time.time() ## CUANTIFICANDO TIEMPO PARA PROCESO DE CADA IMAGEN (INICIO)
 	## LEYENDO UNA IMAGEN
 	image = cv2.imread(f"images/{img}")
 
@@ -20,19 +26,25 @@ for img in images:
 	image_resized = cv2.resize(image, (300,300))
 
 	## PREPROCESAMIENTO
-	blob = cv2.dnn.blobFromImage(image_resized, 1, (300,300), (104, 117, 123))
-	print(blob.shape)
+	blob = cv2.dnn.blobFromImage(image_resized, 1, (300,300), (104, 117, 123)) # PARAMETROS STANDAR PARA LA RED
+	#print(img, " blob shape : ", blob.shape)
 	blob_to_show = cv2.merge([blob[0][0], blob[0][1], blob[0][2]])
 
+	detected_faces = 0
 	## DETECCION
-	net.setInput(blob) ## ENTRADA DE LA RED
+	net.setInput(blob) ## PREPARANDO EL BLOB COMO ENTRADA DE LA RED
 	detections = net.forward()
 	for detection in detections[0][0]:
-		if detection[2] > 0.5: ## CAMBIANDO EL VALOR DE CONFIANZA
-			box = detection[3:7]*[width, height, width, height]
+		if detection[2] > confidence: ## CAMBIANDO EL VALOR DE CONFIANZA
+			box = detection[3:7]*[width, height, width, height] ## SE EXTRAE LAS COORDENADAS DE LA DETECCION
 			x_start, y_start, x_end, y_end = int(box[0]), int(box[1]), int(box[2]), int(box[3])
 			cv2.rectangle(image, (x_start, y_start), (x_end, y_end), (0,0,255), 2)
 			cv2.putText(image, f"{detection[2]*100:.1f}", (x_start, y_start-5), 1, 1.2, (0,0,255), 2)
+			detected_faces += 1
+
+	end = time.time()
+	time_cumulated += end-start
+	print(f'{img}\t{(end-start)*1000:.2f}\t{detected_faces}\t')
 	## MOSTRANDO IMAGEN
 
 	cv2.imshow("Imagen", image)
@@ -41,3 +53,5 @@ for img in images:
 	#cv2.imshow("Imagen pos mean substraction", blob_to_show)
 	cv2.waitKey(0)
 	cv2.destroyAllWindows()
+
+print(f'Tiempo promedio por imagen : {time_cumulated/len(images)*1000:.2f} milisegundos')
